d71e5dc409d634c56f139c7988e222f5c046c030
Rieljun Liguid <me@iamriel.com>
2017-05-18 02:43:06 +0800

    Add School and Education model

--- accounts/models.py
+++ accounts/models.py
@@ -2,13 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Skill(models.Model):
-    title = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.title
-
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
     title = models.CharField(max_length=100, blank=True)
@@ -17,7 +10,7 @@ class UserProfile(models.Model):
     google_plus_url = models.URLField(blank=True)
     linked_in_url = models.URLField(blank=True)
     twitter_url = models.URLField(blank=True)
-    skills = models.ManyToManyField(Skill, through='UserSkill')
+    skills = models.ManyToManyField('Skill', through='UserSkill')
 
     def __str__(self):
         return self.user.get_full_name()
@@ -59,6 +52,13 @@ class Experience(models.Model):
         return '{} - {}'.format(self.user, self.company)
 
 
+class Skill(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
 class UserSkill(models.Model):
     LEVEL_ADVANCED = 'advanced'
     LEVEL_EXPERT = 'expert'
@@ -86,3 +86,24 @@ class UserSkill(models.Model):
     @property
     def title(self):
         return self.skill.title
+
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Education(models.Model):
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    degree = models.CharField(max_length=100, blank=True)
+    field_of_study = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.profile, self.school)
